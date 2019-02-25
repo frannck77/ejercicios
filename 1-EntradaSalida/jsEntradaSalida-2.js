69e5fd8a2176f2ba6e42a7b3f8a23024ca0e6786
frannck77 <frankogustocio@gmail.com>
2019-02-25 11:59:38 -0300

    ejercicios

---
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,8 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+    mostrar = prompt ("cual es tu nombre")
+    alert (mostrar) ;
+}
+
